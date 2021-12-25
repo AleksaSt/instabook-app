3cc67365db0101532ac7f9d0d18f06c33022d0ee
AleksaSt <alexastojcevic@yahoo.com>
2021-12-25 14:15:43 +0100

    register edited

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Gallery from '../views/Gallery.vue'
-import store from '../store/index'
+import ErrorPage from '../views/ErrorPage.vue'
+// import store from '../store/index'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,13 @@ const routes = [
     name: 'Gallery',
     component: Gallery
   },
+  { path: '*',
+    name: 'ErrorPage', 
+    component: ErrorPage 
+  },  
+  // { path: '*',
+  //   redirect: '/404' 
+  // },
   {
     path: '/about',
     name: 'About',
@@ -30,7 +38,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },   
 ]
 
 const router = new VueRouter({
@@ -39,20 +47,24 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  if(to.path != '/login') {
-      if(store.getters.isUserLoggedIn) { 
-          next();
-      } else {
-          next({name: 'Login'});
-      }
-  } else {
-      next();
-  }
+// router.beforeEach((to, from, next) => {
+//   if(to.path != '/login') {
+//       if(store.getters.isUserLoggedIn) { 
+//           next();
+//       } else {
+//           next({name: 'Login'});
+//       }
+//   } else {
+//       next();
+//   }
 
-  if (to.path == '/login' && store.getters.isUserLoggedIn) {
-    next({name: 'Home'})
-  }
-});
+//   if (to.path == '/login' && store.getters.isUserLoggedIn) {
+//     next({name: 'Home'})
+//   }
+
+  // if (to.path == '*') {
+  //   next({name: 'ErrorPage'})
+  // }
+// });
 
 export default router
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,49 +7,34 @@
       hide-footer
       style="font-family: 'Montserrat', sans-serif;"
     >
-    <b-form class="form" @submit="preventDefault()">
-      <b-form-group
-        id="input-group-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="user.email"
-          type="email"
-          placeholder="email"
-          required
-          style="border-radius:2px;font-size:13px;"
-        ></b-form-input>
-      </b-form-group>
+      <b-form class="form" @submit="preventDefault()">
+        <b-form-group
+          id="input-group-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="user.email"
+            type="email"
+            placeholder="email"
+            required
+            style="border-radius:2px;font-size:13px;"
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-group-2" class="mt-2">
-        <b-form-input
-          id="input-2"
-          v-model="user.password"
-          type="password"
-          placeholder="password"
-          required
-          style="border-radius:2px;font-size:13px;"
-        ></b-form-input>
-      </b-form-group>
-      <div style="text-align:center">
-        <b-button class="login-button"  variant="btn btn-primary" @click="loginUser()">Signup</b-button>
-      </div>
-    </b-form>
-      <!-- <div>
-        <div class="form-group">
-          <label>Email</label>
-          <input class="form-control" type="text" v-model="user.email" @click="removeErrorEmail()">
-          <small v-if="errors.email" style="color:red;">{{errors.email[0]}}</small>
+        <b-form-group id="input-group-2" class="mt-2">
+          <b-form-input
+            id="input-2"
+            v-model="user.password"
+            type="password"
+            placeholder="password"
+            required
+            style="border-radius:2px;font-size:13px;"
+          ></b-form-input>
+        </b-form-group>
+        <div style="text-align:center">
+          <b-button class="login-button" variant="btn btn-primary" @click="register()">Signup</b-button>
         </div>
-        <div class="form-group">
-          <label>Password</label>
-          <input class="form-control" type="password" v-model="user.password" @click="removeErrorPassword()">
-          <small v-if="errors.password" style="color:red;">{{errors.password[0]}}</small>
-        </div>
-        <div class="form-group">
-          <button @click="register()" class="btn btn-primary">Submit</button>
-        </div>
-      </div> -->
+      </b-form>
     </b-modal>
 </template>
 
